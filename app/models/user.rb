e622b07ac691b817cec6650ee26b4ca79936ce9e
bamsi <hibamsi@gmail.com>
2022-08-27 15:45:16 +0300

    update model user_id to author_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_many :post
-  has_many :comment
+  has_many :comments, foreign_key: 'author_id'
 
-  # A method that returns the 3 most recent posts for a given user.
+  #A method that returns the 3 most recent posts for a given user.
   def recent_posts(user_id)
     User.find(user_id).post.last(5)
   end
 end
+
