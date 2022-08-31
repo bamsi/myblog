f5d08917a73985f92cca0eb5f5a9b2c8e20c8f2a
bamsi <hibamsi@gmail.com>
2022-08-31 20:11:49 +0300

    add resources to controller action

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,13 +3,7 @@ class UserController < ApplicationController
   def index
   end
 
-  def show(user_id)
-  end
-
-  def show_posts(user_id)
-  end
-
-  def show_post(user_id, post_id)
+  def show
   end
 
 end
