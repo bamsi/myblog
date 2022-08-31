bcf0b51ff0c3905c5f9070f7e9aad82e25a65744
bamsi <hibamsi@gmail.com>
2022-08-31 19:27:07 +0300

    add user controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+class UserController < ApplicationController
+
+  def index
+  end
+
+  def show(user_id)
+  end
+
+  def show_posts(user_id)
+  end
+
+  def show_post(user_id, post_id)
+  end
+
+end
