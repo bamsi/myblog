280947408950979ea3622f11ffd342a011dd056b
haji <hibamsi@gmail.com>
2022-09-09 11:17:31 +0300

    fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :title, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true}, comparison: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true}, comparison: { greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   after_save :update_post
 
