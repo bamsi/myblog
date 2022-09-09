2d5592dcecc0259774a2f02bba3ff873aeced6c4
haji <hibamsi@gmail.com>
2022-09-09 12:46:41 +0300

    update validation controllers

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,39 +2,40 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
   before do
-    @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 1)
+    @user.save!
   end
 
   describe 'GET #index' do
     it 'returns a 200 status code' do
-      subject { get :index }
-      expect(subject).to have_http_status(200)
+      get users_path
+      expect(response).to have_http_status(200)
     end
     it 'render post template' do
-      subject { get :index, params: { user_id: @user.id } }
-      expect(subject).to render_template('index')
+      get users_path
+      expect(response).to render_template('index')
     end
 
     it 'the response body includes correct placeholder' do
-      subject { get :index, params: { user_id: @user.id } }
-      expect(subject.body).to include('Here is a list of users')
+      get users_path
+      expect(response.body).to include('Here is a list of users')
     end
   end
 
   describe 'GET #show' do
     it 'returns a 200 status code' do
-      subject { get :show, params: { id: @user.id } }
-      expect(subject).to have_http_status(200)
+      get user_path(id: @user.id)
+      expect(response).to have_http_status(200)
     end
 
     it 'render show template' do
-      subject { get :show, params: { id: @user.id } }
-      expect(subject).to render_template('show')
+      get user_path(id: @user.id)
+      expect(response).to render_template('show')
     end
 
     it 'the response body includes correct placeholder' do
-      subject { get :index, params: { user_id: @user.id } }
-      expect(subject.body).to include('Here is the details for a user')
+      get user_path(id: @user.id)
+      expect(response.body).to include('Here is the details for a user')
     end
   end
 end
