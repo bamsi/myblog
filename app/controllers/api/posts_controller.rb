2b802c27e231ce028f2ea5f6717d0f9e15082b31
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-16 17:07:27 +0200

    Add api to handle posts

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,24 @@
+class API::V1::PostsController < ApplicationController
+    before_action :set_post, only: %i[show]
+    def index
+      @user = User.find(params[:user_id]).includes(:posts)
+      @posts = @user.posts
+      if @posts.empty?
+        render json: { message: 'No posts yet' }, status: :not_found
+      else
+        render json: @posts
+      end
+    end
+    def show
+      @post = Post.find(params[:id]).includes(:comments)
+      @comments = @post.comments
+      if @comments.empty?
+        render json: { message: 'No comments yet' }, status: :not_found
+      else
+        render json: @comments
+      end
+    end
+    def set_post
+      @post = Post.find(params[:id])
+    end
+end
